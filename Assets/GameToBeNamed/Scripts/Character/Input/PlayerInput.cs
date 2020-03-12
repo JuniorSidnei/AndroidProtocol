9b35dd107a348b72fa5a30950d40bf0d1b80616f
Sidnei <sidneicarrarojunior@gmail.com>
2020-03-12 18:12:35 -0300

    -Adicionar Rewired, -Animações do jogador

--- Assets/GameToBeNamed/Scripts/Character/Input/PlayerInput.cs
+++ Assets/GameToBeNamed/Scripts/Character/Input/PlayerInput.cs
@@ -1,18 +1,29 @@
 ﻿using UnityEngine;
+using Rewired;
+
 
 namespace GameToBeNamed.Character {
     
     [System.Serializable]
     public class PlayerInput : InputSource {
+
+        public int PlayerID = 0;
+        private Player m_player;
+
+        public PlayerInput() {
+            m_player = ReInput.players.GetPlayer(PlayerID);
+        }
+        
         
         public override void Update() {
             CheckButtonDown("Jump", InputAction.Button1);
             CheckButton("MoveRight", InputAction.Button2);
-            CheckButton("MoveLeft", InputAction.Button3);            
+            CheckButton("MoveLeft", InputAction.Button3);           
         }
 
+        
         private void CheckButton(string buttonName, InputAction actionValue) {
-            if (Input.GetButton(buttonName)) {
+            if (m_player.GetButton(buttonName)) {
                 SetAction(actionValue);
             }
             else {
@@ -21,7 +32,7 @@ namespace GameToBeNamed.Character {
         }
         
         private void CheckButtonDown(string buttonName, InputAction actionValue) {
-            if (Input.GetButtonDown(buttonName)) {
+            if (m_player.GetButtonDown(buttonName)) {
                 SetActionDown(actionValue);
             }
             else {
@@ -30,7 +41,7 @@ namespace GameToBeNamed.Character {
         }
         
         private void CheckButtonUp(string buttonName, InputAction actionValue) {
-            if (Input.GetButtonUp(buttonName)) {
+            if (m_player.GetButtonUp(buttonName)) {
                 SetActionUp(actionValue);
             }
             else {
