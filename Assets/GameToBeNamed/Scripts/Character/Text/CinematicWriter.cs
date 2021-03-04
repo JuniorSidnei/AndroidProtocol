a84f4a88e60a9ce41e0d1622d4aa2960d2683e72
sidnei <sidnei@opalastudios.com>
2021-03-04 00:20:44 -0300

    menu animations + introduction scene buttons working

--- Assets/GameToBeNamed/Scripts/Character/Text/CinematicWriter.cs
+++ Assets/GameToBeNamed/Scripts/Character/Text/CinematicWriter.cs
@@ -9,31 +9,45 @@ using UnityEngine.SceneManagement;
 namespace GameToBeNamed.Character {
 
     public class CinematicWriter : MonoBehaviour {
-        [SerializeField]
-        private AudioClip m_textTyping;
+        [Header("Audio typing")]
+        [SerializeField] private AudioClip m_textTyping;
         
+        [Header("Text settings")]
         public Conversation CinematicConversation;
         public TextMeshProUGUI TextConversation;
+
+        [Header("Buttons")]
+        public GameObject NextButton;
+        public GameObject SkipButton;
         
+        [Header("Transition")]
         [SerializeField] private TransitionImageController m_transitionImageController;
 
+        //TODO: FAZER TRANSIÇÃO PARA A CENA DO GAME E AJUSTAR O CALLBACK DA ULTIMA FALA OU DEIXAR FALA EM BRANCO
         private void Start() {
             m_transitionImageController.transitionOut(() => {
                 CinematicConversation.Initialize((message) => {
                     this.WriteText(TextConversation, message, m_textTyping, () => { 
-                        //change to next scene});
+                        NextButton.SetActive(true);
                     });
-                });
+                }, null, cutSpeedch);
             });
+
+            StartCoroutine(showSkipButton());
         }
 
-        private void Update() {
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                CinematicConversation.Next();
-            }
-            else if (Input.GetKeyDown(KeyCode.Escape)) {
-                //change to next scene
-            }
+        public void nextDialog() {
+            CinematicConversation.Next();
+            NextButton.SetActive(false);
+        }
+
+        public void cutSpeedch() {
+            SceneManager.LoadScene("Laboratory");
+        }
+
+        private IEnumerator showSkipButton() {
+            yield return new WaitForSeconds(3f);
+            SkipButton.SetActive(true);
         }
     }
 }
--- Assets/GameToBeNamed/Scripts/Utils/MainMenuController.cs
+++ Assets/GameToBeNamed/Scripts/Utils/MainMenuController.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 namespace GameToBeNamed.Utils {
@@ -10,7 +12,13 @@ namespace GameToBeNamed.Utils {
 
         [SerializeField] private TransitionImageController m_transitionImageController;
         [SerializeField] private CanvasGroup m_menuPanel;
-        
+        [SerializeField] private TextMeshProUGUI m_mainTittle;
+        [SerializeField] private TextMeshProUGUI m_subTittle;
+
+        private void Start() {
+            StartCoroutine(showMainTittle());
+        }
+
         public void startGame() {
             m_menuPanel.DOFade(0, 0.2f).SetEase(Ease.InQuad);
             m_transitionImageController.transitionIn();
@@ -18,6 +26,13 @@ namespace GameToBeNamed.Utils {
         
         public void quitGame () {
             Application.Quit(0);
+        }
+
+        private IEnumerator showMainTittle() {
+            yield return new WaitForSeconds(0.5f);
+            m_mainTittle.DOFade(1, 0.5f);
+            yield return new WaitForSeconds(0.2f);
+            m_subTittle.DOFade(1, 0.5f);
         }
     }
 }
